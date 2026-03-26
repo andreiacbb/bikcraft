b125ab3406edb94c2fef1c610e48474392e3ad91
Andreia <andreiabarbosa@outlook.com>
2026-03-26 01:08:22 -0300

    otimizado para producao

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+}
+
+parametros.forEach(ativarProduto);
